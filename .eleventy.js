896e0dec911bce7bf7afdfb502800be144b73bef
Levin Paßlick <lp@deltacity.net>
2021-04-13 22:07:51 +0200

    :construction: use proper path for assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,6 @@
-const path = require('path');
 
 module.exports = function (config) {
   config.addPassthroughCopy('src/images')
-  config.addFilter('relativeUrl', (url, page) => {
-    if (!url.startsWith('/')) {
-      throw new Error('URL is already relative')
-    }
-    const relativeUrl = path.relative(page.filePathStem, url)
-    return path.relative('..', relativeUrl)
-  })
   return {
     dir: {
       input: 'src',
