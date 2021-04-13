e2f5b0a5f739c46e880ef3c3e8b15f7f9e262a2d
Levin Paßlick <lp@deltacity.net>
2021-04-13 22:07:20 +0200

    :construction: use proper path for assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 module.exports = function (config) {
   config.addPassthroughCopy('src/images')
+  config.addFilter('relativeUrl', (url, page) => {
+    if (!url.startsWith('/')) {
+      throw new Error('URL is already relative')
+    }
+    const relativeUrl = path.relative(page.filePathStem, url)
+    return path.relative('..', relativeUrl)
+  })
   return {
     dir: {
       input: 'src',
@@ -7,3 +16,4 @@ module.exports = function (config) {
     }
   }
 }
+
